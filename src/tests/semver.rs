3e8e4e990fb032518244ff5e7496a05b928aaf10
Jacob Heider <jacob@tea.xyz>
2023-01-29 03:41:08 -0500

    implement arbitrary length semvers and letter warts

--- src/tests/semver.rs
+++ src/tests/semver.rs
@@ -1,26 +1,32 @@
-use crate::semver;
+use crate::semver::Semver;
 use anyhow::Result;
 
 #[test]
 fn test_parse() -> Result<()> {
-    let a = semver::Semver::parse("1.2.3");
-    let b = semver::Semver::parse("1.2.4");
-    let c = semver::Semver::parse("1.2.four");
+    let a = Semver::parse("1.2.3");
+    let b = Semver::parse("1.2.4");
+    let c = Semver::parse("1.2.four");
+    let d = Semver::parse("1.1.1q");
 
     assert!(a.is_ok());
     assert!(b.is_ok());
     assert!(c.is_err());
+    assert!(d.is_ok());
+
+    let d = d?;
 
     assert_eq!(a?.raw, "1.2.3");
     assert_eq!(b?.raw, "1.2.4");
+    assert_eq!(d.raw, "1.1.1q");
+    assert_eq!(d.components, [1, 1, 1, 17]);
 
     Ok(())
 }
 
 #[test]
 fn test_compare() -> Result<()> {
-    let a = semver::Semver::parse("1.2.3")?;
-    let b = semver::Semver::parse("1.2.4")?;
+    let a = Semver::parse("1.2.3")?;
+    let b = Semver::parse("1.2.4")?;
 
     assert!(a.eq(&a));
     assert!(!a.eq(&b));
@@ -33,6 +39,21 @@ fn test_compare() -> Result<()> {
 
     assert!(a.lt(&b));
     assert!(!b.lt(&a));
+
+    let c = Semver::parse("1.1.1q")?;
+    let d = Semver::parse("1.1.1s")?;
+
+    assert!(c.eq(&c));
+    assert!(!c.eq(&d));
+
+    assert!(!c.neq(&c));
+    assert!(c.neq(&d));
+
+    assert!(!c.gt(&d));
+    assert!(d.gt(&c));
+
+    assert!(c.lt(&d));
+    assert!(!d.lt(&c));
 
     Ok(())
 }
